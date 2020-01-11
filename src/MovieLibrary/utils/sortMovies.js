e6b9f4902deccf11795d1442def54efcc403559a
Eugene Musika <eugene.musika@cpamatica.io>
2020-01-11 17:30:51 +0200

    finish

--- src/MovieLibrary/utils/sortMovies.js
+++ src/MovieLibrary/utils/sortMovies.js
@@ -7,13 +7,13 @@
 export default function sortMovies (movies, order) {
 	switch (order) {
 		case 'name_asc':
-			return movies.sort((a, b) => a.title.localeCompare(b.title));
+			return [...movies].sort((a, b) => a.title.localeCompare(b.title));
 
 		case 'name_desc':
-			return movies.sort((a, b) => -a.title.localeCompare(b.title));
+			return [...movies].sort((a, b) => -a.title.localeCompare(b.title));
 
 		case 'rating':
-			return movies.sort((a, b) => b.vote_average - a.vote_average);
+			return [...movies].sort((a, b) => b.vote_average - a.vote_average);
 	}
 
 	return movies;
